ff940d67117554d0c2baaf43cc5f700c77eac678
rampxxxx <rampxxxx@gmail.com>
2012-03-12 18:33:18 +0100

    Guarda solo en fichero los parametros indicados en el fichero de configuracion.

---
+++ source/src/mod_leconf.f90
@@ -0,0 +1,205 @@
+! ------------------------------------------------------------------------------
+module mod_leconf
+! ------------------------------------------------------------------------------
+  use mod_precision
+  use mod_UnitUtil
+  use mod_readcir
+  !.
+  type t_msk
+    private
+    integer(ip), allocatable :: dat_i(:)
+    real(rp),    allocatable :: dat_r(:)
+  end type t_msk
+  !.
+  type t_stm
+    private
+    integer(ip)          :: ntrain
+    real(rp), allocatable    :: data(:)
+  end type t_stm
+  !.
+  type, public:: t_cnf
+    integer(ip)           :: ict
+    integer(ip)           :: post(2)
+    real(rp)              :: step(3)
+    type(t_msk)           :: prm
+    type(t_stm)           :: stm
+    integer(ip), allocatable :: file_parameters_i(:)
+  end type t_cnf
+! ------------------------------------------------------------------------------
+contains
+! ------------------------------------------------------------------------------
+subroutine readconfile (cnf)
+! ------------------------------------------------------------------------------
+  implicit none
+  type(t_cnf)           :: cnf
+  !.
+  
+  integer(ip), parameter   :: MAX=100
+  integer(ip)              :: lu_cfg, n, m, io_err
+  integer(ip)              :: dat_i(MAX)
+  real(rp)                 :: dat_r(MAX)
+  character (len = 256)    :: fname,strout
+  logical                  :: flag
+  integer(ip)              :: file_parameters_i(MAX)
+
+  call a_unit(lu_cfg)
+  call fun_getarg (1,'-i',fname)  !.Nombre del archivo de configuracion
+  m=len_trim(fname)
+  open(unit=lu_cfg, file=fname(1:m), status='old')
+  !.
+  do 
+    call srch_lec (lu_cfg,'#',strout, flag, io_err)
+    !.
+    if (flag) then
+      if     (index(strout,'#MODEL')  == 1) then
+        !.
+        read (lu_cfg, * ,iostat=io_err) cnf%ict
+        if (io_err > 0) stop '>>>.Error reading the cell type'
+        !.
+      elseif (index(strout,'#PARAMETERS') == 1) then
+        !.
+        read (lu_cfg, * ,iostat=io_err) n,(dat_i(m),m=1,n),(dat_r(m),m=1,n)
+        if (io_err > 0) stop '>>>.Error reading parameter mask and values'
+        allocate (cnf%prm%dat_i(n))
+        allocate (cnf%prm%dat_r(n))
+        cnf%prm%dat_i(1:n)=dat_i
+        cnf%prm%dat_r(1:n)=dat_r
+        !.
+      elseif (index(strout,'#STEP') == 1) then
+        !.
+        read (lu_cfg, * ,iostat=io_err) (cnf%step(m),m=1,3)
+        if (io_err > 0) stop '>>>.Error reading step information'
+        !.
+      elseif (index(strout,'#STIMULUS')    == 1) then
+        !.
+        read (lu_cfg,*,iostat=io_err) n
+        cnf%stm%ntrain = n
+        if (io_err > 0) stop '>>>.Error reading the number of stimulus'
+        allocate (cnf%stm%data(4*n))
+        read (lu_cfg, * ,iostat=io_err) (cnf%stm%data(m),m=1,4*n)
+        if (io_err > 0) stop '>>>.Error reading stimulus'
+      elseif (index(strout,'#POST')     == 1) then 
+        !.
+        read (lu_cfg, * ,iostat=io_err) (cnf%post(m),m=1,2)
+        if (io_err > 0) stop '>>>.Error reading postprocess frequency'
+        !.
+      elseif (index(strout,'#FILE_PARAMETERS') == 1) then
+        !.
+        read (lu_cfg, * ,iostat=io_err) n,(file_parameters_i(m),m=1,n)
+        if (io_err > 0) stop '>>>.Error reading file parameters mask and values'
+        allocate (cnf%file_parameters_i(n))
+        cnf%file_parameters_i(1:n)=dat_i
+        !.
+      else
+          if (index(strout,'#END')     == 1) exit
+      endif
+    else
+      if (io_err == -1) exit
+    endif
+  end do
+  !.
+  call close_unit (lu_cfg)
+  !.
+  return
+end subroutine readconfile
+! ------------------------------------------------------------------------------
+function get_celltype (cnf) result(ict)
+! ------------------------------------------------------------------------------
+ type(t_cnf)            :: cnf     
+ integer(ip)            :: ict
+
+ ict = cnf%ict
+ return
+end function get_celltype
+! ------------------------------------------------------------------------------
+function get_number_of_prm(prm) result(n)
+! ------------------------------------------------------------------------------
+  implicit none
+  type(t_msk), intent(in) :: prm
+  integer(ip)             :: n
+
+  n = size(prm%dat_i)
+  if (n<0) then
+    n=0
+  endif
+end function get_number_of_prm
+! ------------------------------------------------------------------------------
+subroutine get_mask(prm, dat_i, dat_r)
+! ------------------------------------------------------------------------------
+  implicit none
+  type(t_msk), intent(in) :: prm
+  integer(ip), intent(out):: dat_i(:)
+  real(rp),    intent(out):: dat_r(:)
+  integer(ip)             :: n
+
+  n = size(prm%dat_i)
+  if (n>0) then
+      dat_i(1:n) = prm%dat_i
+      dat_r(1:n) = prm%dat_r
+  endif
+end subroutine get_mask
+! ------------------------------------------------------------------------------
+subroutine get_step(cnf, vec)
+! ------------------------------------------------------------------------------
+  implicit none
+  type(t_cnf),intent(in):: cnf
+  real(rp),intent(out)  :: vec(:)
+
+  vec(:) = cnf%step
+  
+  return
+end subroutine get_step
+! ------------------------------------------------------------------------------
+function get_init_save(cnf) result(init_save)
+! ------------------------------------------------------------------------------
+  implicit none
+  type(t_cnf),intent(in) :: cnf
+  integer(ip)            :: init_save
+
+  init_save = cnf%post(1)
+  
+  return
+end function get_init_save
+! ------------------------------------------------------------------------------
+function get_post_freq(cnf) result(outfreq)
+! ------------------------------------------------------------------------------
+  implicit none
+  type(t_cnf),intent(in) :: cnf
+  integer(ip)            :: outfreq
+
+  outfreq = cnf%post(2)
+  
+  return
+end function get_post_freq
+! ------------------------------------------------------------------------------
+function get_stimulus(stm, t, tend) result  (Istm)
+! ------------------------------------------------------------------------------
+  implicit none
+  type(t_stm),intent(in) :: stm
+  real(rp),intent(in)    :: t, tend 
+  real(rp)               :: Istm
+  integer(ip)            :: j,ntrain
+  real(rp)               :: tist, tfst
+
+  Istm = 0.0_rp
+  ntrain = stm%ntrain
+  j=0
+  tist = stm%data(1)
+  do while (tist .le. tend)
+    tfst = tist+stm%data(4*j+3)
+    if((t>=tist).and.(t<=tfst)) then
+         Istm = -stm%data(4*j+4)
+         exit
+    endif
+    tist=tist+stm%data(4*j+2)
+    if(j<(ntrain-1)) then
+        if (tist >= stm%data(4*(j+1)+1)) then
+            j=j+1
+            tist=stm%data(4*j+1)
+        endif
+    endif
+  enddo
+  return
+end function get_stimulus
+! ------------------------------------------------------------------------------
+end module mod_leconf
---
+++ source/src/test_main.f90
@@ -0,0 +1,129 @@
+! -------------------------------------------------------------------------------
+program test_main
+! -------------------------------------------------------------------------------
+  use mod_precision
+  use mod_leconf
+  use mod_UnitUtil
+  use mod_Iion
+  !.
+  implicit none
+
+  type(t_cnf)               :: cnf
+  character(len=256)        :: outname
+  integer(ip), parameter    :: mxprm=100
+  integer(ip)               :: lu_curr,lu_stat,iteration
+  integer(ip)               :: aux, nprm, ict, outfreq, init_save
+  real(rp)                  :: step(3), Istm, U, time, Qion
+  real(rp)                  :: t1,t2,v_prm(mxprm)
+  real(rp), allocatable     :: v_ve(:)
+  real(rp), allocatable     :: v_cr(:)
+  real(rp), allocatable     :: dat_r(:)
+  integer(ip), allocatable  :: dat_i(:)
+  integer                   :: numeroDeParametro
+
+  !.
+
+  write(*,*) 'Begining reading ...'
+  call readconfile(cnf)
+  write(*,*) 'Done reading ...'
+  ict = get_celltype(cnf)           ! Retrieve cell type
+  call PrintNumCellEM ('Solving ',ict,ict)
+  nprm = get_number_of_prm(cnf%prm) ! Retrieve mask
+  write(*,*) 'Number of param to modified : ',nprm
+  if (nprm>0) then
+      allocate(dat_r(nprm))
+      allocate(dat_i(nprm))
+      call get_mask(cnf%prm, dat_i, dat_r)
+      write(*,*) 'Mask:'
+      write(*,10) 'dat_i: ', (dat_i(aux),aux=1,nprm)
+      write(*,20) 'dat_r: ', (dat_r(aux),aux=1,nprm)
+  endif
+  call get_step(cnf,step)   ! Retrieve simulation time and increment
+  write(*,*) 'Step'
+  write(*,30) 'Tini: ', step(1)
+  write(*,30) 'DT  : ', step(2)
+  write(*,30) 'Tfin: ', step(3)
+  outfreq =  get_post_freq(cnf) 
+  init_save= get_init_save(cnf) 
+  write(*,40) 'Saving starts at increment: ',init_save
+  write(*,40) 'OutputFreq: ',outfreq
+
+  call fun_getarg(3,'-o',outname) !.Retrieve root name for outputfile
+  aux=len_trim(outname)
+  call a_unit(lu_curr) 
+  call a_unit(lu_stat) 
+  open(lu_curr,FILE=outname(1:aux)//'_curr.dat');
+  open(lu_stat,FILE=outname(1:aux)//'_stat.dat');
+
+  iteration =0;
+  time = step(1);
+  ! Initialicing state variables and parameters
+  aux = get_numstatevar(ict)
+  allocate(v_ve(aux))
+  v_ve(1:aux)=get_ic_em(ict,aux)
+  aux = get_numcur(ict)
+  allocate(v_cr(aux))
+  call get_parameter_nd(ict,v_prm,aux);
+  if(nprm>0) then
+      v_prm(dat_i(1:nprm))=dat_r(1:nprm)
+  endif
+  U = initcond_elecmodel (ict) 
+
+  call get_time(t1)
+  do while (time < step(3))
+    Istm = get_stimulus(cnf%stm, time, step(3))
+!
+    if(iteration.ge.init_save) then
+      if(mod(iteration,outfreq) == 0) then
+              write(*,'(i,$)') iteration
+              write(*,*) "time=>" , time
+              write(*,*) "U=> " , U
+              write(*,*) "v_ve(1:1)=> ", v_ve(1:1)
+              write(*,*) "v_ve(2:2)=> ", v_ve(2:2)
+              ! write to file only the selected params or all if any.
+              if(size(cnf%file_parameters_i) == 0) then
+                      write(lu_stat,50) time, U, v_ve
+                      write(lu_curr,50) time, v_cr
+              else
+                      do numeroDeParametro=1 , size(cnf%file_parameters_i)
+                      if(numeroDeParametro==1) then
+                              if(size(cnf%file_parameters_i) == 1) then
+                                      write(lu_stat,50) time, U , v_ve(numeroDeParametro:numeroDeParametro)
+                                      write(lu_curr,50) time,     v_cr(numeroDeParametro:numeroDeParametro)
+                              else
+                                      write(lu_stat,90) time, U , v_ve(numeroDeParametro:numeroDeParametro)
+                                      write(lu_curr,90) time,     v_cr(numeroDeParametro:numeroDeParametro)
+                              end if
+                      else
+                              write(lu_stat,95) v_ve(numeroDeParametro:numeroDeParametro)
+                              write(lu_curr,95) v_cr(numeroDeParametro:numeroDeParametro)
+                      end if
+                      enddo ! loop for every param.
+              end if ! params or not.
+      end if
+    endif
+!
+    call get_Iion (ict,step(2),Istm,0.0_rp,U,0.0_rp,Qion, &
+                   v_prm(1:nprm),v_ve, v_cr)
+    U = U - step(2) * (Qion+Istm);
+    time = time + step(2);
+    iteration = iteration + 1;
+!
+  enddo
+  call get_time(t2)
+  write(*,60) 'Elapsed time:',t2-t1,'s'
+  close(lu_curr)
+  close(lu_stat)
+
+10 format (A,40(2X,I3))
+20 format (A,40(2X,F12.6))
+30 format (A,2X,F16.2)
+40 format (A,2X,I15)
+50 format (F15.2,50(2X,F15.10))
+60 format (A,2X,F16.3,2X,A)
+90 format (F15.2,50(2X,F15.10,$))
+95 format (50(2X,F15.10))
+!60 format (F14.6,F12.6)
+end program test_main
+
+
